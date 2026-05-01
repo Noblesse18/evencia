6520603d0120dfa2f4e04febc6de9700eb256fbe
Noblesse18 <cnsamy02@gmail.com>
2026-05-01 17:37:34 +0200

    feature : generation d'apk pour le mobile

--- mobile/src/api/client.js
+++ mobile/src/api/client.js
@@ -4,7 +4,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 // pour emulateur android : 10.0.2.2:5000
 // pour appareil reel : ip de ma machine : 192.168.1.42/24
 
-const API_BASE_URL = 'http://10.0.2.2:5000/api'; // 10.0.2.2 = localhost du host dans l'emulateur Android
+const API_BASE_URL = 'https://evencia.onrender.com/api'; // 10.0.2.2 = localhost du host dans l'emulateur Android
 
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
